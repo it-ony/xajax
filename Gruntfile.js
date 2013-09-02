969b99feadfb02cc0f7196f8c5ae3d9f6d53fb80
Tony Findeisen <github@it-ony.de>
2013-09-02 16:10:09 +0200

    added build step for minifying

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,33 @@ module.exports = function (grunt) {
     // Project configuration
     grunt.initConfig({
         uglify: {
-            options: {
-                beautify: {
-                    "max_line_len": 100
-                }
-            },
-            my_target: {
+            build: {
                 files: {
+                    'server.min.js': ['server.js'],
                     'client.min.js': ['client.js']
                 }
             }
         }
     });
 
+    grunt.registerTask("minifyHtml", function() {
+        var fs = require("fs"),
+            index, server;
+
+        index = fs.readFileSync("index.html", "utf8");
+        server = fs.readFileSync("server.min.js", "utf8");
+
+        index = index.replace(/<script.*?script>/, '<script type="text/javascript">' + server +'</script>');
+
+        fs.writeFileSync("index.min.html", index, "utf8");
+
+    });
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
     // Default task
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['uglify:my_target']);
+    grunt.registerTask('build', ['uglify:build', 'minifyHtml']);
 
 };
