2e4f97f05d3f5ee70e66cf84971b18de5b2426c6
Tony Findeisen <github@it-ony.de>
2013-09-02 13:24:02 +0200

    implemented handling of receiving messages

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 (function(window, undefined) {
 
+    var parent = window.parent,
+        console = window.console,
+        initialized = false,
+        handlers = {
+            ajax: function(data, messageId) {
+
+            }
+        };
+
     if (window.addEventListener) {
         addEventListener("message", receiveMessage, false);
     } else {
@@ -8,13 +17,47 @@
 
     function receiveMessage(event) {
         console.log(event);
+
+        if (!initialized) {
+            return;
+        }
+
+        var eventData = JSON.parse(event.data);
+
+        // validate event data
+        if (!(eventData && eventData.hasOwnProperty("type") && eventData.hasOwnProperty("data") && eventData.hasOwnProperty("messageId"))) {
+            sendMessage(eventData.messageId, "Data couldn't be parsed");
+            return;
+        }
+
+        var handler = handlers[eventData.type];
+
+        if (handler) {
+            try {
+                // invoke handler
+                handler(eventData.data, eventData.messageId);
+            } catch (e) {
+                sendMessage(eventData.messageId, e);
+            }
+        } else {
+            sendMessage(eventData.messageId, "Handler for type '" + eventData.type + "' not found");
+        }
     }
 
-    var parent = window.parent,
-        console = window.console;
+    function sendMessage(messageId, err, data) {
+
+        var message = {
+            error: err,
+            data: data,
+            messageId: messageId
+        };
+
+        parent.postMessage(JSON.stringify(message), "*");
+    }
 
     if (parent) {
         parent.postMessage("initialized", "*");
+        initialized = true;
     } else {
         console && console.log && console.log("Cannot find parent");
     }
